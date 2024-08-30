09a33ef764bda499a587f25b2bafc35e91fbb423
Noah Penza <penzanoah@gmail.com>
2024-08-30 22:09:56 +1000

    Revert "replacing a tags with link tags in AppNavBar"
    This reverts commit 3fcb50286f3989740d918431ee6b43dc5bcaca26.

--- template/app/src/client/components/AppNavBar.tsx
+++ template/app/src/client/components/AppNavBar.tsx
@@ -1,6 +1,5 @@
-import { routes } from 'wasp/client/router';
+import { Link, routes } from 'wasp/client/router';
 import { useAuth } from 'wasp/client/auth';
-import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { Dialog } from '@headlessui/react';
 import { BiLogIn } from 'react-icons/bi';
@@ -16,6 +15,8 @@ const navigation = [
   { name: 'AI Scheduler (Demo App)', href: routes.DemoAppRoute.build() },
   { name: 'File Upload (AWS S3)', href: routes.FileUploadRoute.build() },
   { name: 'Pricing', href: routes.PricingPageRoute.build() },
+  { name: 'Documentation', href: DocsUrl },
+  { name: 'Blog', href: BlogUrl },
 ];
 
 const NavLogo = () => <img className='h-8 w-8' src={logo} alt='Your SaaS App' />;
@@ -28,9 +29,9 @@ export default function AppNavBar() {
     <header className='absolute inset-x-0 top-0 z-50 shadow sticky bg-white bg-opacity-50 backdrop-blur-lg backdrop-filter dark:border dark:border-gray-100/10 dark:bg-boxdark-2'>
       <nav className='flex items-center justify-between p-6 lg:px-8' aria-label='Global'>
         <div className='flex lg:flex-1'>
-          <Link to='/' className='-m-1.5 p-1.5'>
+          <a href='/' className='-m-1.5 p-1.5'>
             <img className='h-8 w-8' src={logo} alt='My SaaS App' />
-          </Link>
+          </a>
         </div>
         <div className='flex lg:hidden'>
           <button
@@ -44,16 +45,14 @@ export default function AppNavBar() {
         </div>
         <div className='hidden lg:flex lg:gap-x-12'>
           {navigation.map((item) => (
-            <Link
+            <a
               key={item.name}
-              to={item.href}
+              href={item.href}
               className='text-sm font-semibold leading-6 text-gray-900 duration-300 ease-in-out hover:text-yellow-500 dark:text-white'
             >
               {item.name}
-            </Link>
+            </a>
           ))}
-          <a href={DocsUrl}>Documentation</a>
-          <a href={BlogUrl}>Blog</a>
         </div>
         <div className='hidden lg:flex lg:flex-1 gap-3 justify-end items-center'>
           <ul className='flex justify-center items-center gap-2 sm:gap-4'>
@@ -61,11 +60,11 @@ export default function AppNavBar() {
           </ul>
 
           {isUserLoading ? null : !user ? (
-            <Link to={!user ? routes.LoginRoute.build() : routes.AccountRoute.build()} className='text-sm font-semibold leading-6 ml-4'>
+            <a href={!user ? routes.LoginRoute.build() : routes.AccountRoute.build()} className='text-sm font-semibold leading-6 ml-4'>
               <div className='flex items-center duration-300 ease-in-out text-gray-900 hover:text-yellow-500 dark:text-white'>
                 Log in <BiLogIn size='1.1rem' className='ml-1 mt-[0.1rem]' />
               </div>
-            </Link>
+            </a>
           ) : (
             <div className='ml-4'>
               <DropdownUser user={user} />
@@ -77,10 +76,10 @@ export default function AppNavBar() {
         <div className='fixed inset-0 z-50' />
         <Dialog.Panel className='fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white dark:text-white dark:bg-boxdark px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
           <div className='flex items-center justify-between'>
-            <Link to='/' className='-m-1.5 p-1.5'>
+            <a href='/' className='-m-1.5 p-1.5'>
               <span className='sr-only'>Your SaaS</span>
               <NavLogo />
-            </Link>
+            </a>
             <button
               type='button'
               className='-m-2.5 rounded-md p-2.5 text-gray-700 dark:text-gray-50'
@@ -94,25 +93,15 @@ export default function AppNavBar() {
             <div className='-my-6 divide-y divide-gray-500/10'>
               <div className='space-y-2 py-6'>
                 {navigation.map((item) => (
-                  <Link
+                  <a
                     key={item.name}
-                    to={item.href}
+                    href={item.href}
                     onClick={() => setMobileMenuOpen(false)}
                     className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 dark:text-white hover:dark:bg-boxdark-2'
                   >
                     {item.name}
-                  </Link>
+                  </a>
                 ))}
-                <a 
-                  href={DocsUrl} 
-                  className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 dark:text-white hover:dark:bg-boxdark-2'>
-                    Documentation
-                </a>
-                <a 
-                  href={BlogUrl} 
-                  className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 dark:text-white hover:dark:bg-boxdark-2'>
-                    Blog
-                </a>
               </div>
               <div className='py-6'>
                 {isUserLoading ? null : !user ? (
